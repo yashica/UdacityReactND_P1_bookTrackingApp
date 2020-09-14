882f616361ffd98ba4654ce23b6b5e6ccfd27959
Magdalena Bloeckner <magdalenabloeckner@magdalenas-mbp.speedport_w_724v_typ_a_05011603_06_003>
2020-09-14 12:17:10 +0200

    Add BookShelfChanger component

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import BookShelfChanger from './BookShelfChanger'
 
 class Book extends Component {
     render() {
@@ -13,19 +14,7 @@ class Book extends Component {
               backgroundImage: `url( ${ coverimgURL })`
             }}
           />
-          <div className="book-shelf-changer">
-            <select>
-              <option value="move" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">
-                Currently Reading
-              </option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+          <BookShelfChanger />
         </div>
         <div className="book-title">
           { title }
