8c780837f7d266ce78a6f009e976e798d533ec03
Magdalena Bloeckner <magdalenabloeckner@magdalenas-mbp.speedport_w_724v_typ_a_05011603_06_003>
2020-09-15 10:27:48 +0200

    Working on SearchPage - before installing escape-string-regexp sort-by

--- src/MainPage.js
+++ src/MainPage.js
@@ -3,127 +3,7 @@ import { Link } from 'react-router-dom'
 import BookShelf from "./BookShelf";
 
 class MainPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      /**
-       * TODO: Instead of using this state variable to keep track of which page
-       * we're on, use the URL in the browser's address bar. This will ensure that
-       * users can use the browser's back and forward buttons to navigate between
-       * pages, as well as provide a good URL they can bookmark and share.
-       */
-      //showSearchPage: false,
-      books_currentlyReading: [
-        {
-          title: `To Kill a Mockingbird`,
-          author: `Harper Lee`,
-          coverimgURL: `http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api`,
-        },
-        {
-          title: `Ender's Game`,
-          author: `Orson Scott Card`,
-          coverimgURL: `http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api`,
-        },
-      ],
-  
-      books_wantToRead: [
-        {
-          title: `1776`,
-          author: `David McCullough`,
-          coverimgURL: `http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api`,
-        },
-        {
-          title: `Harry Potter and the Sorcerer's Stone`,
-          author: `J.K. Rowling`,
-          coverimgURL: `http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api`,
-        },
-      ],
-  
-      books_read: [
-        {
-          title: `The Hobbit`,
-          author: `J.R.R. Tolkien`,
-          coverimgURL: `http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api`,
-        },
-        {
-          title: `Oh, the Places You'll Go!`,
-          author: `Seuss`,
-          coverimgURL: `http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api`,
-        },
-        {
-          title: `The Adventures of Tom Sawyer`,
-          author: `Mark Twain`,
-          coverimgURL: `http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api`,
-        },
-      ],
-      books_other: [],
-  
-    };
-  }
-  
-
-  moveBook = (book, shelf_from, shelf_to) => {
-    this.removeBookFromShelf(book,shelf_from);
-    this.addBookToShelf(book,shelf_to);
-    return 0;
-  }
-
-  removeBookFromShelf = (book,shelfID) => {
-    switch(shelfID) {
-      case 'currentlyReading':
-        //alert(`Book ${book.title} will be removed from books_currentlyReading`);
-        this.setState((currentState) => ({
-          books_currentlyReading: currentState.books_currentlyReading.filter((b) => {
-            return b.title !== book.title
-          })
-        }))
-        break;
-      case 'wantToRead':
-        //alert(`Book ${book.title} will be removed from books_wantToRead`);
-        this.setState((currentState) => ({
-          books_wantToRead: currentState.books_wantToRead.filter((b) => {
-            return b.title !== book.title
-          })
-        }))
-        break;
-      case 'read':
-        //alert(`Book ${book.title} will be removed from books_red`);
-        this.setState((currentState) => ({
-          books_read: currentState.books_read.filter((b) => {
-            return b.title !== book.title
-          })
-        }))
-        break;
-      default:
-        return 0;
-    }
-  }
-
-  addBookToShelf = (book,shelfID) => {
-    switch(shelfID) {
-      case 'currentlyReading':
-        //alert(`Book ${book.title} will be added to books_currentlyReading`);
-        this.setState((currentState) => ({
-          books_currentlyReading: currentState.books_currentlyReading.concat([book])
-        }))
-        break;
-      case 'wantToRead':
-        //alert(`Book ${book.title} will be added to books_wantToRead`);
-        this.setState((currentState) => ({
-          books_wantToRead: currentState.books_wantToRead.concat([book])
-        }))
-        break;
-      case 'read':
-        //alert(`Book ${book.title} will be added to books_red`);
-        this.setState((currentState) => ({
-          books_read: currentState.books_read.concat([book])
-        }))
-        break;
-      default:
-        return 0;
-    }
-  }
-  
+    
   render() {
     return (
       <div className="list-books">
@@ -135,20 +15,20 @@ class MainPage extends Component {
             <BookShelf 
               caption='Currently Reading' 
               shelfID='currentlyReading' 
-              books={ this.state.books_currentlyReading}
-              onMoveBook={this.moveBook}
+              books={ this.props.books_currentlyReading}
+              onMoveBook={this.props.onMoveBook}
             />
             <BookShelf 
               caption='Want to Read' 
               shelfID='wantToRead' 
-              books={ this.state.books_wantToRead }
-              onMoveBook={this.moveBook}
+              books={ this.props.books_wantToRead }
+              onMoveBook={this.props.onMoveBook}
             />
             <BookShelf 
               caption='Read' 
               shelfID='read' 
-              books={ this.state.books_read }
-              onMoveBook={this.moveBook}
+              books={ this.props.books_read }
+              onMoveBook={this.props.onMoveBook}
             />
           </div>
         </div>
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,52 +1,79 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import BooksGrid from "./BooksGrid";
 
 class SearchPage extends Component {
-    render() {
-        return (
-            <div className="search-books">
-            <div className="search-books-bar">
-              <Link
-                className="close-search"
-                to="/"
-                /*onClick={() => this.setState({ showSearchPage: false })}*/
-                /*onClick = { this.props.onNavigate }*/
-              >
-                Close
-              </Link>
-              <div className="search-books-input-wrapper">
-                {
-                  //NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  //You can find these search terms here:
-                  //https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+  state = {
+    query: '',
+    books: []
+  }
 
-                  //However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  //you don't find a specific author or title. Every search is limited by search terms.
-                }
-                <input type="text" placeholder="Search by title or author" />
-              </div>
-            </div>
-            <div className="search-books-results">
-              <BooksGrid shelf={ "none" } books={[{
-                  title: `The Hobbit`,
-                  author: `J.R.R. Tolkien`,
-                  coverimgURL: `http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api`,
-                },
-                {
-                  title: `Oh, the Places You'll Go!`,
-                  author: `Seuss`,
-                  coverimgURL: `http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api`,
-                },
-                {
-                  title: `The Adventures of Tom Sawyer`,
-                  author: `Mark Twain`,
-                  coverimgURL: `http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api`,
-                },]} />
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query.trim()
+    }))
+
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then((books) => {
+      this.setState(() => ({
+        books: books
+      }))
+      console.log(books);
+    })
+  }
+
+  moveBookToShelf = (book, shelf_from, shelf_to) => {
+    this.setState((currentState) => ({
+      books: currentState.books.filter((b) => {
+        return b.title !== book.title
+      })
+    }))
+    this.props.onMoveBook(book, shelf_from, shelf_to)
+    return 0;
+  }
+
+  render() {
+      return (
+          <div className="search-books">
+          <div className="search-books-bar">
+            <Link
+              className="close-search"
+              to="/"
+            >
+              Close
+            </Link>
+            <div className="search-books-input-wrapper">
+              {
+                JSON.stringify(this.state.query)
+                //NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                //You can find these search terms here:
+                //https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                //However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                //you don't find a specific author or title. Every search is limited by search terms.
+              }
+              <input 
+                type="text" 
+                placeholder="Search by title or author"
+                value={this.state.query}
+                onChange={(event) => this.updateQuery(event.target.value)} 
+              />
             </div>
           </div>
-        );
-    }
+          <div className="search-books-results">
+            <BooksGrid 
+              shelf={ "none" } 
+              books={ this.state.books }
+              onMoveBook={ this.moveBookToShelf } 
+              />
+          </div>
+        </div>
+      );
+  }
 }
 
 export default SearchPage
